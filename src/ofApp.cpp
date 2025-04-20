6b0aaa02f810c92e814140471345581078c2efab
Adam Baranec <abaranec@gmail.com>
2025-04-20 18:11:20 +0200

    new version

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -3,62 +3,38 @@
 //--------------------------------------------------------------
 void ofApp::setup(){
 	//oscReceiver.setup("192.168.50.113", 8000);
-	ofSoundStreamSettings settings;
+	/*ofSoundStreamSettings settings;
 	settings.numOutputChannels = 0;
 	settings.numInputChannels = 6;
 	settings.bufferSize = 256;
 	//Focusrite USB Asio
 	settings.setInDevice(soundStream.getDeviceList()[4]);
 	settings.setInListener(this);
-	soundStream.setup(settings);
+	soundStream.setup(settings);*/
 	//camera is set to 1920x1080 resolution
+	ofLoadImage(tex, "vj-loop-5-hd-texture.jpg");
 	camera.setGlobalPosition(0, 0, 1);
 	camera.setFov(45);
 	camera.setNearClip(0.1);
 	camera.setFarClip(1000);
-	shader.load("vertex.vert", "fragment.frag");
+	shader.load("vertex.vert", "texViewer.frag");
 	shaderViewer.setUseVbo(true);
 	shaderViewer.setPosition(glm::vec3(0.0, 0.0, 0.0));
 	shaderViewer.setHeight(1);
-	shaderViewer.setWidth((1920.0f / 1080.0f));
+	shaderViewer.setWidth((float)ofGetWidth() / (float)ofGetHeight());
 	ofHideCursor();
-	ofSetFrameRate(60);
 }
 
 //--------------------------------------------------------------
 void ofApp::update() {
-	int numFrames = ofGetFrameNum();
-	if (numFrames != 0) {
-		if (numFrames % (60*60*3) == 0) {
-			if (var == 13) { var = -1; }
-			var += 1;
-		}
-	}
 }
 //--------------------------------------------------------------
 void ofApp::draw(){
 	camera.begin();
 	shader.begin();
-	shader.setUniform1f("time", ofGetElapsedTimef());
-	shader.setUniform1f("amp", amplitude);
-	shader.setUniform1f("aspect", (float)ofGetWidth() / ofGetHeight());
-	shader.setUniform1i("var", var);
-	/*shader.setUniform1i("circlesParams.quantity", circlesParams.quantity);
-	shader.setUniform1f("circlesParams.amount", circlesParams.amount);
-	shader.setUniform1f("circlesParams.hueRange", circlesParams.hueRange);
-	shader.setUniform3f("circlesParams.hsvColor", circlesParams.hsvColor);
-	shader.setUniform1i("circlesParams.direction", circlesParams.direction);
-	shader.setUniform1i("circlesParams.monochrome", circlesParams.monochrome);
-	shader.setUniform1i("circlesParams.coordModification", circlesParams.coordModification);
-	shader.setUniform1i("circlesParams.visible", circlesParams.visible);
-	shader.setUniform1i("circlesParams.mode", circlesParams.mode);
-	shader.setUniform1i("circlesParams.index", circlesParams.index);
-
-	shader.setUniform1f("ringsParams.amount", ringsParams.amount);
-	shader.setUniform1i("ringsParams.visible", ringsParams.visible);
-	shader.setUniform1i("ringsParams.mode", ringsParams.mode);
-	shader.setUniform1i("ringsParams.index", ringsParams.index);
-	shader.setUniform3f("ringsParams.hsvColor", ringsParams.hsvColor);*/
+	//shader.setUniformTexture("tex", tex, 0);
+	//shader.setUniform1f("time", ofGetElapsedTimef());
+	//shader.setUniform1f("aspect", (float)ofGetWidth() / (float)ofGetHeight());
 	shaderViewer.draw();
 	shader.end();
 	camera.end();
@@ -123,5 +99,5 @@ void ofApp::dragEvent(ofDragInfo dragInfo){
 //--------------------------------------------------------------
 void ofApp::audioIn(ofSoundBuffer& buffer)
 {
-	amplitude = buffer.getRMSAmplitudeChannel(5);
+	//amplitude = buffer.getRMSAmplitudeChannel(5);
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "ofMain.h"
-#include "ofxOsc\src\ofxOsc.h"
+//#include "ofxOsc\src\ofxOsc.h"
 
 class ofApp : public ofBaseApp {
 
@@ -26,74 +26,7 @@ public:
 	ofSoundStream soundStream;
 	ofCamera camera;
 	ofShader shader;
-	ofPlanePrimitive shaderViewer, imgViewer;
-	ofFbo shaderTex;
-	ofTexture tex0, tex1, tex2, tex3, sphereTex0, sphereTex1;
-	ofColor back;
-	ofSpherePrimitive sphere;
-	ofxOscReceiver oscReceiver;
-	float amplitude;
-	int activeSources;
-	int var = 0;
-	struct CirclesParams {
-		int index = 0;
-		int visible = 0;
-		int mode = 0;
-		float amount = 1.0f;
-		int direction = 0;
-		int monochrome = 0;
-		int coordModification = 0;
-		int quantity = 10;
-		float hueRange = 0.1f;
-		glm::fvec3 hsvColor = glm::fvec3(0.0f, 1.0f, 1.0f);
-	} circlesParams, circlesParamsFromOsc;
-
-	struct RingsParams {
-		int index = 0;
-		int visible = 0;
-		int mode = 0;
-		float amount = 1.0f;
-		float density = 15.0f;
-		float speed = 5.0f;
-		int direction = 1;
-		glm::fvec3 hsvColor = glm::fvec3(0.0f, 1.0f, 1.0f);
-	} ringsParams, ringsParamsFromOsc;
-
-	struct SquaresParams{
-		int index = 0;
-		int visible = 0;
-		int mode = 0;
-		float amount = 1.0f;
-		int repeatX = 1;
-		int repeatY = 1;
-		float scrollX = 0.0f;
-		float scrollY = 0.0f;
-		int outline = 0;
-		int rotate = 0;
-		int rotateLeft = 0;
-		int rotateRight = 0;
-		glm::fvec3 hsvColor = glm::fvec3(0.0f, 1.0f, 1.0f);
-	} squaresParams, squaresParamsFromOsc;
-	
-	struct SolidParams {
-		int index = 0;
-		int visible = 0;
-		int mode = 0;
-		float amount = 1.0f;
-		glm::fvec3 hsvColor = glm::fvec3(0.0f, 1.0f, 1.0f);
-		int showTexture = 0;
-		int textureIndex;
-	} solidParams, solidParamsFromOsc;
-
-	struct OscillatorParams {
-		int index = 0;
-		int visible = 0;
-		int mode = 0;
-		float amount = 1.0f;
-		glm::fvec3 hsvColor1 = glm::fvec3(0.0f, 1.0f, 1.0f);
-		glm::fvec3 hsvColor2 = glm::fvec3(0.0f, 1.0f, 1.0f);
-		int addBall = 0;
-		int predefined = 0;
-		int predefinedOscIndex;
-	} oscParams, oscParamsFromOsc;
+	ofPlanePrimitive shaderViewer;
+	ofTexture tex;
+	int bank, var;
 };
