2734374f679e3e9ab54975e4973c1f06a6935215
Adam Baranec <35851756+adambaranec@users.noreply.github.com>
2025-09-05 19:44:50 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 bool isDigits(std::string& str)
 {
 	for (char ch : str) {
-		int v = ch; // ASCII Val converted
+		int v = ch;
 		if (!(ch >= 48 && ch <= 57)) {
 			return false;
 			break;
@@ -17,11 +17,10 @@ bool isDigits(std::string& str)
 //========================================================================
 int main(int argc, char* argv[]) {
 
-	//Use ofGLFWWindowSettings for more options like multi-monitor fullscreen
 	ofGLWindowSettings settings;
 	settings.setSize(1920, 1080);
 	settings.setGLVersion(4,6);
-	settings.windowMode = OF_WINDOW; //can also be OF_FULLSCREEN
+	settings.windowMode = OF_WINDOW;
 
 	ofDirectory loopsDir("loops");
 	loopsDir.allowExt("mp4");
@@ -83,24 +82,3 @@ int main(int argc, char* argv[]) {
 		}
 	}
 }
-
-/*
-int numSlices = 40;  // Number of divisions around the torus
-int numSegments = 20; // Number of divisions along the tube
-float R = 200;       // Major radius
-float r = 50;        // Minor radius
-
-for (int i = 0; i < numSlices; i++) {
-	float theta = ofMap(i, 0, numSlices, 0, TWO_PI);
-	for (int j = 0; j < numSegments; j++) {
-		float phi = ofMap(j, 0, numSegments, 0, TWO_PI);
-
-		// Parametric equations for torus
-		float x = (R + r * cos(phi)) * cos(theta);
-		float y = (R + r * cos(phi)) * sin(theta);
-		float z = r * sin(phi);
-
-		torus.addVertex(ofVec3f(x, y, z));
-		torus.addNormal(ofVec3f(x, y, z).normalized());
-	}
-}*/
